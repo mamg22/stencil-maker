8d25379147e91f2f12cc959cbc7f88f51ffce2c5
mamg22 <45301823+mamg22@users.noreply.github.com>
2024-06-26 00:04:55 -0400

    Properly calculate font size and positioning

--- src/stencil_maker/core.py
+++ src/stencil_maker/core.py
@@ -1,21 +1,34 @@
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 
-def calculate_image_size(text: str, font: ImageFont.FreeTypeFont, margin: int) -> tuple[int, int]:
+def textbbox(text: str, font: ImageFont.FreeTypeFont, spacing: int) -> tuple[int, int, int, int]:
     tmp_draw = ImageDraw.ImageDraw(Image.new("1", (0, 0)))
 
-    bounding_box = tmp_draw.multiline_textbbox((0, 0), text, font=font)
-    return (margin*2 + bounding_box[2], margin*2 + bounding_box[3])
+    return tmp_draw.multiline_textbbox((0, 0), text, font=font, spacing=spacing)
+
+def calculate_image_size(bbox: tuple[int, int, int, int], margin: int) -> tuple[int, int]:
+    x0, y0, x1, y1 = bbox
+    
+    return (abs(x1 - x0) + margin * 2, abs(y1 - y0) + margin * 2)
 
 def draw_base_image(text: str, font_filename: str, font_size: int) -> Image.Image:
-    margin = round(font_size / 10)
     font = ImageFont.truetype(font_filename, size=font_size)
+    full_height = sum(abs(val) for val in font.getmetrics())
+    margin = full_height // 5
+    spacing = full_height // 5
 
-    img_size = calculate_image_size(text, font, margin)
+    bbox = textbbox(text, font, spacing)
+
+    img_size = calculate_image_size(bbox, margin)
 
     image = Image.new("L", img_size, color="white")
     draw = ImageDraw.ImageDraw(image)
 
-    draw.multiline_text((margin, 0), text, font=font, fill="black")
+    x0, y0, *_ = bbox
+
+    x_offset = margin - x0
+    y_offset = margin - y0
+
+    draw.multiline_text((x_offset, y_offset), text, font=font, fill="black", spacing=spacing)
 
     return image
 
