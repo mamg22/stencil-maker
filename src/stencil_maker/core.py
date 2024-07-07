8c7bfc2567aabe2f002458f9832c13e2c108bae3
mamg22 <45301823+mamg22@users.noreply.github.com>
2024-07-07 14:59:43 -0400

    Better text drawing with custom line positioning
    This commit stops using Pillow's multiline_text function since it
    doesn't handle line spacing properly when text includes characters with
    descenders (lowercase 'g' and 'y') or ascenders (Accented letters or
    some characters which may be taller than 'A'), which doesn't play well
    when the program needs to work with arbitrary fonts while avoiding
    overlapping.
    
    This algorithm positions the lines directly below another with only a
    fixed spacing in between, disregarding any spacing the font uses for
    smaller and lowercase characters.

--- src/stencil_maker/core.py
+++ src/stencil_maker/core.py
@@ -1,40 +1,122 @@
+from typing import NamedTuple, Self
+
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 
-def textbbox(text: str, font: ImageFont.FreeTypeFont, spacing: int) -> tuple[int, int, int, int]:
-    tmp_draw = ImageDraw.ImageDraw(Image.new("1", (0, 0)))
+dummy_draw = ImageDraw.ImageDraw(Image.new("1", (0, 0)))
 
-    return tmp_draw.multiline_textbbox((0, 0), text, font=font, spacing=spacing)
+type Color = float | tuple[int, ...] | str
 
-def calculate_image_size(bbox: tuple[int, int, int, int], margin: int) -> tuple[int, int]:
-    x0, y0, x1, y1 = bbox
-    
-    return (abs(x1 - x0) + margin * 2, abs(y1 - y0) + margin * 2)
 
-def draw_base_image(text: str, font_filename: str, font_size: int) -> Image.Image:
+class BoundingBox(NamedTuple):
+    x0: int
+    y0: int
+    x1: int
+    y1: int
+
+    def transformed(
+        self, move_x: int = 0, move_y: int = 0, resize_x: int = 0, resize_y: int = 0
+    ) -> Self:
+        return self.__class__(
+            self.x0 + move_x,
+            self.y0 + move_y,
+            self.x1 + move_x + resize_x,
+            self.y1 + move_y + resize_y,
+        )
+
+    @property
+    def width(self) -> int:
+        return abs(self.x1 - self.x0)
+
+    @property
+    def height(self) -> int:
+        return abs(self.y1 - self.y0)
+
+
+def calculate_image_size(bbox: BoundingBox, margin: int) -> tuple[int, int]:
+    return (bbox.width + margin * 2, bbox.height + margin * 2)
+
+
+def draw_base_image(
+    text: str,
+    font_filename: str,
+    font_size: int,
+    stroke_width: int = 0,
+    background_color: Color = "white",
+    fill_color: Color = "black",
+    stroke_color: Color = "black",
+) -> Image.Image:
     font = ImageFont.truetype(font_filename, size=font_size)
-    full_height = sum(abs(val) for val in font.getmetrics())
-    margin = full_height // 5
-    spacing = full_height // 5
+    ascent, descent = (abs(val) for val in font.getmetrics())
+    full_height = ascent + descent
 
-    bbox = textbbox(text, font, spacing)
+    margin = max(abs(full_height // 10), 1)
+    spacing = margin
+
+    boxes: list[tuple[str, BoundingBox]] = []
+
+    for line in text.splitlines():
+        bbox = dummy_draw.textbbox(
+            (0, 0), line, font, stroke_width=stroke_width, anchor="lt"
+        )
+        bounding = BoundingBox(*bbox)
+        boxes.append((line, bounding))
+
+    x0 = min(box.x0 for _, box in boxes)
+    y0 = min(box.y0 for _, box in boxes)
+    x1 = max(box.x1 for _, box in boxes)
+    y1 = sum(box.height for _, box in boxes) + spacing * (len(boxes) - 1)
+
+    bbox = BoundingBox(x0, y0, x1, y1).transformed(margin, margin)
 
     img_size = calculate_image_size(bbox, margin)
 
-    image = Image.new("L", img_size, color="white")
+    image = Image.new("RGB", img_size, color=background_color)
     draw = ImageDraw.ImageDraw(image)
 
-    x0, y0, *_ = bbox
+    offset = margin
+    for text, box in boxes:
+        adjbox = box.transformed(margin - box.x0, offset - box.y0)
 
-    x_offset = margin - x0
-    y_offset = margin - y0
+        draw.text(
+            adjbox.transformed(-box.x0, -box.y0),
+            text,
+            font=font,
+            fill=fill_color,
+            spacing=spacing,
+            stroke_width=stroke_width,
+            stroke_fill=stroke_color,
+            anchor="lt",
+        )
 
-    draw.multiline_text((x_offset, y_offset), text, font=font, fill="black", spacing=spacing)
+        offset += box.height + spacing
 
     return image
 
 
-def make_stencil(text: str, font_filename: str, font_size: int, flip: bool = False) -> Image.Image:
-    image = draw_base_image(text, font_filename, font_size)
+def make_stencil(
+    text: str,
+    font_filename: str,
+    font_size: int,
+    flip: bool = False,
+    stroked: bool = True,
+) -> Image.Image:
+    if stroked:
+        fill = "white"
+        stroke_width = max(font_size // 50, 1)
+        stroke_color = "black"
+    else:
+        fill = "black"
+        stroke_width = 0
+        stroke_color = "white"
+
+    image = draw_base_image(
+        text,
+        font_filename,
+        font_size,
+        stroke_width=stroke_width,
+        fill_color=fill,
+        stroke_color=stroke_color,
+    )
 
     if flip:
         image = ImageOps.mirror(image)
