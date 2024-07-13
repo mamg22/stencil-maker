95da0590e95b69ed8f9653cbeb5211ecc447ead8
mamg22 <45301823+mamg22@users.noreply.github.com>
2024-07-13 17:30:44 -0400

    Format code

--- src/stencil_maker/__init__.py
+++ src/stencil_maker/__init__.py
@@ -1,3 +1,3 @@
 from .core import make_stencil
 
-__all__ = ['make_stencil']
+__all__ = ["make_stencil"]
--- src/stencil_maker/__main__.py
+++ src/stencil_maker/__main__.py
@@ -6,13 +6,20 @@ from stencil_maker import make_stencil
 
 eprint = partial(print, file=sys.stderr)
 
-def main():
-    parser = argparse.ArgumentParser(description="Simple tool to generte a stencil for text provided in stdin")
 
-    parser.add_argument('-f', '--font', required=True, help="Font to write text with")
-    parser.add_argument('-s', '--size', type=int, default=300, help="Font size in pixels")
-    parser.add_argument('-F' , '--flip', action='store_true', help="Flip final image horizontally")
-    parser.add_argument('filename', help="Filename to save the image to")
+def main():
+    parser = argparse.ArgumentParser(
+        description="Simple tool to generte a stencil for text provided in stdin"
+    )
+
+    parser.add_argument("-f", "--font", required=True, help="Font to write text with")
+    parser.add_argument(
+        "-s", "--size", type=int, default=300, help="Font size in pixels"
+    )
+    parser.add_argument(
+        "-F", "--flip", action="store_true", help="Flip final image horizontally"
+    )
+    parser.add_argument("filename", help="Filename to save the image to")
 
     args = parser.parse_args()
 
@@ -21,7 +28,7 @@ def main():
     if not text:
         eprint("No text provided")
         return
-    
+
     try:
         image = make_stencil(text, args.font, args.size, flip=args.flip)
     except OSError as err:
@@ -29,10 +36,11 @@ def main():
         return
 
     try:
-        image.save(args.filename, optimize=True, format='png')
+        image.save(args.filename, optimize=True, format="png")
     except (OSError, ValueError) as err:
         eprint(err)
         return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
